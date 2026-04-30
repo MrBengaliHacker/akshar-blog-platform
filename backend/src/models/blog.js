36c64ad97a1e4b098e62312ab7b294a21549f81b
RitamMondal2468 <ritammondal700144@gmail.com>
2026-04-30 12:34:03 +0530

    feat(blog): add blog model with slug, banner, author and status fields

---
+++ backend/src/models/blog.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      maxlength: [180, 'Title must be less than 180 characters'],
+    },
+    slug: {
+      type: String,
+      required: [true, 'Slug is required'],
+      unique: [true, 'Slug must be unique'],
+    },
+    content: {
+      type: String,
+      required: [true, 'Content is required'],
+    },
+    banner: {
+      publicId: {
+        type: String,
+        required: [true, 'Banner public ID is required'],
+      },
+      url: {
+        type: String,
+        required: [true, 'Banner URL is required'],
+      },
+      width: {
+        type: Number,
+        required: [true, 'Banner width is required'],
+      },
+      height: {
+        type: Number,
+        required: [true, 'Banner height is required'],
+      },
+    },   
+    author: {
+      type: Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Author is required'],
+    },
+    viewsCount: {
+      type: Number,
+      default: 0,
+    },
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
+    commentsCount: {
+      type: Number,
+      default: 0,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ['draft', 'published'],
+        message: '{VALUE} is not supported',
+      },
+      default: 'draft',
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'publishedAt',
+    },
+  }
+);
+
+module.exports = mongoose.model('Blog', blogSchema);
