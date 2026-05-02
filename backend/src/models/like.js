db0360814ca5bf9268bc06f87916e0b6aac2c873
RitamMondal2468 <ritammondal700144@gmail.com>
2026-05-02 12:52:35 +0530

    feat(like): add like model with blog and comment references

---
+++ backend/src/models/like.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema(
+  {
+    blogId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+    commentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Like', likeSchema);
